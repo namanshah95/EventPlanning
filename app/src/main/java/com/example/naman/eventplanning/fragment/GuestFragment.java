836d161b118b0194f65bec1cbca9442bb00ce650
mengdidi <441348693@qq.com>
2017-03-25 18:22:11 -0400

    add welcome page

---
+++ app/src/main/java/com/example/naman/eventplanning/fragment/GuestFragment.java
@@ -0,0 +1,9 @@
+package com.example.naman.eventplanning.fragment;
+
+/**
+ * Created by mengdili on 3/25/17.
+ */
+
+public class GuestFragment {
+
+}
---
+++ app/src/main/java/com/example/naman/eventplanning/home_activity.java
@@ -0,0 +1,59 @@
+package com.example.naman.eventplanning;
+
+/**
+ * Created by mengdili on 3/23/17.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.example.naman.eventplanning.R;
+
+
+public class home_activity extends AppCompatActivity {
+
+    @BindView(R.id.top_img)
+    ImageView topImg;
+    @BindView(R.id.rl_bottom)
+    RelativeLayout rlBottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentLayout());
+        ButterKnife.bind(this);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(6000);
+                    Intent intent = new Intent();
+
+                        intent.setClass(home_activity.this, MainActivity.class);
+
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) {// Interrupted
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+
+    protected int getContentLayout() {
+        return R.layout.activity_firstpage;
+    }
+
+}
