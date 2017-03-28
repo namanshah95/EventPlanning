457f4d9dd4c5d4b7bdf12c6758ab13d849cb15b9
Chad Miller <chad.miller.3333@gmail.com>
2017-03-27 22:41:08 -0400

    Added entity-searching by name and email

--- web/api/controllers/entities.php
+++ web/api/controllers/entities.php
@@ -1,14 +1,70 @@
 <?
     namespace entities;
 
-    $API->get( '/entities/{entity}', 'entities\get_entity' );
-    $API->post( '/entities/',        'entities\add_entity' );
+    $API->get( '/entities/',         'entities\get_entities' );
+    $API->get( '/entities/{entity}', 'entities\get_entity'   );
+    $API->post( '/entities/',        'entities\add_entity'   );
 
-    function __firebase_fetch( $ext_firebase_id )
+    function get_entities( $request, $response, $args )
     {
+        $params = $request->getQueryParams();
+
+        $name  = array_key_exists( 'Name', $params )  ? $params['Name']  : false;
+        $email = array_key_exists( 'Email', $params ) ? $params['Email'] : false;
+
         $firebase = new \Firebase\FirebaseLib( constant( 'FIREBASE_URL' ), constant( 'FIREBASE_AUTHKEY' ) );
-        $record   = $firebase->get( "/users/$ext_firebase_id" );
-        return $record;
+        $record   = $firebase->get( "/users/" );
+        $record   = json_decode( $record, true );
+
+        $result_set   = [];
+        $firebase_ids = [];
+
+        foreach( array_keys( $record ) as $firebase_id )
+            $firebase_ids[] = "'$firebase_id'";
+
+        $firebase_ids_str = implode( ',', $firebase_ids );
+
+        $query = <<<SQL
+select ext_firebase_id,
+       entity
+  from tb_entity
+ where ext_firebase_id in ($firebase_ids_str)
+SQL;
+
+        $resource = query_execute( $query );
+
+        if( query_success( $resource ) )
+        {
+            $entities_pre = query_fetch_all( $resource );
+            $entities     = [];
+
+            foreach( $entities_pre as $entity )
+                $entities[$entity['ext_firebase_id']] = $entity['entity'];
+        }
+        else
+            return database_error( $response );
+
+        foreach( $record as $firebase_id => $json )
+        {
+            if(
+                   array_key_exists( $firebase_id, $entities )
+                && (
+                        ( !$name && !$email )
+                     || ( $name  && $json['Name']  == $name )
+                     || ( $email && $json['Email'] == $email )
+                   )
+              )
+            {
+                $firebase_ids[] = $firebase_id;
+
+                $result                    = $json;
+                $result['ext_firebase_id'] = $firebase_id;
+                $result['entity']          = $entities[$firebase_id];
+                $result_set[]              = $result;
+            }
+        }
+
+        return $response->withJson( $result_set );
     }
 
     function get_entity( $request, $response, $args )
@@ -32,7 +88,9 @@ SQL;
                 return object_not_found_error( $response, 'tb_entity', $entity );
 
             $ext_firebase_id = $record['ext_firebase_id'];
-            $record          = \entities\__firebase_fetch( $ext_firebase_id );
+
+            $firebase = new \Firebase\FirebaseLib( constant( 'FIREBASE_URL' ), constant( 'FIREBASE_AUTHKEY' ) );
+            $record   = $firebase->get( "/users/$ext_firebase_id" );
 
             $record           = json_decode( $record, true );
             $record['entity'] = $entity;
